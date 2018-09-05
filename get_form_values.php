b6cf249be42a92f8558972e4d5aec340a2f69d94
unknown <davidwampamba@gmail.com>
2018-09-05 19:49:18 +0300

    Clean loop closing and code indent

--- get_form_values.php
+++ get_form_values.php
@@ -7,12 +7,10 @@
 <?php 
     $workPeriods = $_POST;
     $tempWorkPeriods = [];
-    foreach($workPeriods['days'] as $workPeriodDay) :
-            foreach($workPeriods['hrs_from'] as $workPeriodDayhrsFrom):
-                 foreach($workPeriods['hrs_to'] as $workPeriodDayhrsTo):
-                    $tempWorkPeriods[$workPeriodDay] = ['start' => $workPeriodDayhrsFrom,'end' => $workPeriodDayhrsTo];
-                endforeach;
-            endforeach;
-        endforeach;
+    foreach($workPeriods['days'] as $workPeriodDay)
+        foreach($workPeriods['hrs_from'] as $workPeriodDayhrsFrom)
+            foreach($workPeriods['hrs_to'] as $workPeriodDayhrsTo)
+            $tempWorkPeriods[$workPeriodDay] = ['start' => $workPeriodDayhrsFrom,'end' => $workPeriodDayhrsTo];
+
 ?>
 <?php  echo json_encode($tempWorkPeriods); ?>
