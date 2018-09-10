2f1861cf2501e62424e80417812d9dd6bd73ebd6
unknown <davidwampamba@gmail.com>
2018-09-10 17:47:03 +0300

    Search review

---
+++ search.php
@@ -0,0 +1,10 @@
+<?php
+
+$term = $_GET['keywords'];
+$locationFilter = $_GET['locationFilter'];
+
+//SELECT from table WHERE column = $term AND column = $locationFilter
+//Loop results
+//echo json_encode( $result )
+//OR loop while creating an HTML string.
+//echo HTMLString
